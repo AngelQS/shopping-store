64d9cd74156da65e486745e659cbbc5937c09f67
Angel Edwin Quiroz Soriano <developer.aqs@gmail.com>
2021-03-31 00:36:51 -0500

    First project setup

---
+++ src/test/java/com/backlabs/projects/shoppingstore/ShoppingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backlabs.projects.shoppingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
